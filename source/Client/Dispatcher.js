056b1b78b6ffa70f2c346279843081e16b399b9f
Alejandro Sandoval Parra <asp@mail.uni-paderborn.de>
2014-01-30 18:54:30 +0100

    Add comments and formatting to some classes related to communication in the client-side

--- source/Client/Dispatcher.js
+++ source/Client/Dispatcher.js
@@ -1,79 +1,101 @@
 /**
-*    Webarena - A web application for responsive graphical knowledge work
-*
-*    @author Felix Winkelnkemper, University of Paderborn, 2012
-*
-*/
+ * Webarena - A web application for responsive graphical knowledge work
+ * 
+ * @author Felix Winkelnkemper, University of Paderborn, 2012
+ * 
+ * @class Dispatcher 
+ * @classdesc Object holding functions for server communication
+ */
 
 "use strict";
 
-/**
- * @namespace Object holding functions for server communication
- */
-var Dispatcher={};
+
+var Dispatcher = {};
 
 /**
  * List of calls
  */
-var calls={};
+var calls = {};
 
 /**
  * List of waiting response functions
  */
-var responseFunctions={};
+var responseFunctions = {};
 
-Dispatcher.call=function(message){
-	
-	var type=message.name;
-	var data=message.data;
-	
-	if (calls[type]){
-	  	calls[type](data);
-	} else {
-		console.log('ERROR: No function for '+type);
-	}
-	
+var responseCleanupTimeout = false;
+
+Dispatcher.counter = 1;
+
+/**
+ * Manages a call from the server
+ * 
+ * @param {Object} message the received from the server.
+ */
+Dispatcher.call = function(message) {
+    var type = message.name;
+    var data = message.data;
+
+    if (calls[type]) {
+        calls[type](data);
+    } else {
+        console.log('ERROR: No function for ' + type);
+    }
 }
 
-Dispatcher.response=function(message){
-	
-	var id=message.id;
-	var data=message.data;
-	
-	if (responseFunctions[id]){	
-	  	responseFunctions[id](data);
-	  	delete(responseFunctions[id]);
-	} else {
-		console.log('ERROR: No function for '+id);
-	}
+/**
+ * Manages a response from the server
+ * 
+ * @param {Object} message the received from the server.
+ */
+Dispatcher.response = function(message) {
+    var id = message.id;
+    var data = message.data;
+
+    if (responseFunctions[id]) {
+        responseFunctions[id](data);
+        delete (responseFunctions[id]);
+    } else {
+        console.log('ERROR: No function for ' + id);
+    }
 }
 
-var responseCleanupTimeout=false;
-Dispatcher.counter=1;
-Dispatcher.query=function(queryName,queryArguments,responseFunction){
-	
-	this.counter++;
-	var random=this.counter+' '+(new Date().getTime()-1296055327011);
-	var responseID=queryName+random;
+/**
+ * Make a query to the server
+ * 
+ * @param {Object} queryName The type of query
+ * @param {Object} queryArguments The arguments of the query
+ * @param {Object} responseFunction The response function (callback function)
+ */
+Dispatcher.query = function(queryName, queryArguments, responseFunction) {
+    this.counter++;
+    var random = this.counter + ' ' + (new Date().getTime() - 1296055327011);
+    var responseID = queryName + random;
 
-	responseFunctions[responseID]=responseFunction;
-	if (responseCleanupTimeout){
-		window.clearTimeout(responseCleanupTimeout);
-		responseCleanupTimeout=false;
-	}
-	responseCleanupTimeout=window.setTimeout(function(){
-		responseFunction={}; // get rid of all remaining response functions
-	},5000);
-	Modules.SocketClient.sendCall(queryName, queryArguments, responseID);
+    responseFunctions[responseID] = responseFunction;
+    if (responseCleanupTimeout) {
+        window.clearTimeout(responseCleanupTimeout);
+        responseCleanupTimeout = false;
+    }
+    responseCleanupTimeout = window.setTimeout(function() {
+        responseFunction = {}; // get rid of all remaining response functions
+    }, 5000);
+    Modules.SocketClient.sendCall(queryName, queryArguments, responseID);
 }
 
-
-Dispatcher.registerCall=function(type,callFunction){
-	//callfunction signature (socket,data);
-	calls[type]=callFunction;
+/**
+ * Register a new callfunction by type
+ * 
+ * @param {Object} type the type of call function
+ * @param {Object} callFunction the call function to be registered.
+ */
+Dispatcher.registerCall = function(type, callFunction) {
+    //callfunction signature (socket,data);
+    calls[type] = callFunction;
 }
 
-
-Dispatcher.init=function(){
-
+/**
+ * Init function called in index.html to initialize this module
+ */
+Dispatcher.init = function() {
+    // do nothing
 }
--- source/Client/SocketClient.js
+++ source/Client/SocketClient.js
@@ -1,25 +1,47 @@
+/**
+ * Webarena - A web application for responsive graphical knowledge work
+ * 
+ * @author , University of Paderborn, 2012
+ * 
+ */
+
 "use strict";
-var SocketClient={};
+var SocketClient = {};
 
-SocketClient.init=function(){
-	var url=location.protocol+'//'+location.hostname;
-	var socket = io.connect(url);
-	Modules.Socket=socket;
-	socket.on('message', function (data) {
-		//console.log(data);
-		if (data.type=='call') Modules.Dispatcher.call(data);
-		if (data.type=='response') Modules.Dispatcher.response(data);
-	});
-	socket.on('disconnect', function() {
-		GUI.disconnected();
-	});
-	socket.on('connect', function () {
-		GUI.connected();
-	});
+/**
+ * Init function called in index.html to initialize this module
+ */
+SocketClient.init = function() {
+    var url = location.protocol + '//' + location.hostname;
+    var socket = io.connect(url); // <--- Connects to the server: creates a websocket (socket.IO)
+    Modules.Socket = socket;
+    
+    socket.on('message', function(data) {
+        // console.log(data);
+        if (data.type == 'call') Modules.Dispatcher.call(data);
+        if (data.type == 'response') Modules.Dispatcher.response(data);
+    });
+    socket.on('disconnect', function() {
+        GUI.disconnected();
+    });
+    socket.on('connect', function() {
+        GUI.connected();
+    });
 }
 
-SocketClient.sendCall=function(type, data, responseID){
-	Modules.Socket.emit('message',{'type':type,'data':data,'responseID':responseID});
+/**
+ * Sends a message to the server
+ * 
+ * @param {Object} type the type of call.
+ * @param {Object} data the data to be sent to the server.
+ * @param {Object} responseID the response id.
+ */
+SocketClient.sendCall = function(type, data, responseID) {
+    Modules.Socket.emit('message', {
+        'type' : type,
+        'data' : data,
+        'responseID' : responseID
+    });
 }
-  
-SocketClient.serverCall=SocketClient.sendCall;
+
+SocketClient.serverCall = SocketClient.sendCall;
