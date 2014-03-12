33e9414b51dd26edec02b09325fe8779d6ba60a2
Jörg Amelunxen <j.amelunxen@googlemail.com>
2014-03-12 09:18:45 +0100

    added function for pushing testdata to the DB

--- source/Server/RightManager.js
+++ source/Server/RightManager.js
@@ -1,24 +1,96 @@
 var db = require('monk')('localhost/WebArena')
 
-var RightManager = function() {
-  var possibleAccessRights = [];
+var DEBUG_OF_RIGHTMANAGEMENT = true;
+
+var fillCurrentDbWithTestData = function(){
     
-  /**
-  *		The function is needed to initialize the RightManager
-  *
-  */	
-  this.init = function(){
+    if(DEBUG_OF_RIGHTMANAGEMENT){
+        console.log("filling testdata");
+    }
+    
+    var pushRights  = ['1#create','2#read','3#update','4#delete'];
+    var pushUsers   = ['joerg#12345','Vanessa#xyz'];
+    var pushRoles   = ['1#1#RandomGuys#write,read#overwrite#joerg,vanessa',
+                       '2#1#Boss#read#overwrite#vanessa'];
+    
+    /* clear tables */
+    db.get('rights').drop();
+    db.get('users').drop();
+    db.get('roles').drop();
+    
+    /* push test rights */
+    var collection = db.get('rights');
+    pushRights.forEach(function(item){
+                        var token = item.split("#");
+                        collection.insert({id:String(token[0]),name:String(token[1])})
+                       
+                        if(DEBUG_OF_RIGHTMANAGEMENT){
+                            console.log("pushing testright: "+String(token[1]));
+                        }
+                       });
+    
+    /* push test users */
+    collection = db.get('users');
+    pushUsers.forEach(function(item){
+                        var token = item.split("#");
+                        collection.insert({username:String(token[0]),password:String(token[1])});
+                      
+                        if(DEBUG_OF_RIGHTMANAGEMENT){
+                            console.log("pushing testuser: "+String(token[0]));
+                        }
+                       });
+    
+    /* push test roles */
+    collection = db.get('roles');
+    pushRoles.forEach(function(item){
+                      var token = item.split("#");
+                      
+                      var aID        = String(token[0]);
+                      var aContextID = String(token[1]);
+                      var aName      = String(token[2]);
+                      var someRights = String(token[3]).split("#");
+                      var aMode      = String(token[4]);
+                      var someUser   = String(token[5]).split("#");
+                      
+                      collection.insert({id:aID,
+                                        contextID:aContextID,
+                                        name:aName,
+                                        rights:someRights,
+                                        mode:aMode,
+                                        users:someUser});
+                      
+                      if(DEBUG_OF_RIGHTMANAGEMENT){
+                        console.log("pushing testrole: "+aName);
+                      }
+                      });
+};
+
+var RightManager = function() {
+    fillCurrentDbWithTestData();
+    
+    var possibleAccessRights = [];
+    
+    /**
+     *		The function is needed to initialize the RightManager
+     *
+     */
+    this.init = function(){
       /* get all exiting access rights from the database */
       var collection = db.get('rights');
       collection.find({},{},function(e,docs){
                       docs.forEach(function(entry){
-                                   console.log("adding right: "+String(entry.name));
+                                   
+                                   if(DEBUG_OF_RIGHTMANAGEMENT){
+                                    console.log("adding right: "+String(entry.name));
+                                   }
+                                
                                    possibleAccessRights.push(docs.name);
                                    });
                       });
 
 	  console.log("RightManager has been initialized");
-  }
+  };
+    
   /**
    *	The function returns a boolean value that 
    *	represents if the current user has the right 
