f4fa4e9d0e07733bb68b809dff3bd79f5de0306c
Lisa Bunse <lbunse@mail.upb.de>
2014-03-30 11:34:05 +0200

    Modified Rightmanager sidebar: Change content on deselection of objects

--- source/Client/guis.common/javascript/1.rightmanager.js
+++ source/Client/guis.common/javascript/1.rightmanager.js
@@ -4,6 +4,9 @@
 
 GUI.rightmanager = new function() {
   var that = this;
+  
+  /* Content of rightmanager sidebar*/
+  var rm_Content;
 
   this.init = function() {
     console.log("GUI.rightmanager initialized");
@@ -21,68 +24,89 @@ GUI.rightmanager = new function() {
     $("#rmNewRoleButton").click(function(event) {
     	var role = $("#rmNewRoleTextfield").val();
     	console.log(role);
-    });
+    });   
+    
+    /* Initially no object is selected */    
+    rm_Content = $('#rightmanager').html();
+	$("#rightmanager").replaceWith("<div class = \"jDesktopInspector\" id=\"rightmanager\"><div class=\"jDesktopInspector_main\"><div class=\"jDesktopInspector_pageHead\">Object:</div>No Object selected!</div>");
+   
     
   };
 
   this.updateContent = function(theObject) {
 	  
-	  console.log(theObject);
-	  
 	  /* Display selected object information */
 	  
 	 var selectedObjects = ObjectManager.getSelected();
 	  
-	 /* Depending on how many objects are selected display only object information or everything */
+	 /* Depending on how many objects are selected display nothing, only object information or everything */
 	 
-	 if(selectedObjects.length > 1){
-		 $("#rm_rolesHead").hide();
-		 $("#rm_rolesPage").hide();
-		 $("#rm_rightsHead").hide();
-		 $("#rm_rightsPage").hide();	
-		 $("#rm_usersHead").hide();
-		 $("#rm_usersPage").hide();
+	 	/* No object selected */
+		if(selectedObjects.length == 0){
+			
+			/* Store current content of rightmanager sidebar and replace it */
+			rm_Content = $('#rightmanager').html();
+			$("#rightmanager").replaceWith("<div class = \"jDesktopInspector\" id=\"rightmanager\"><div class=\"jDesktopInspector_main\"><div class=\"jDesktopInspector_pageHead\">Object:</div>No Object selected!</div>");
+		
+		}
+		
+		/* More than one object selected */
+		else if(selectedObjects.length > 1){			
+			
+			 /* Replace current content of rightmanager sidebar */
+			 $("#rightmanager").replaceWith("<div id=\"rightmanager\" class = \"jDesktopInspector\">"+rm_Content+"</div>");			 
+			
+			 $("#rm_rolesHead").hide();
+			 $("#rm_rolesPage").hide();
+			 $("#rm_rightsHead").hide();
+			 $("#rm_rightsPage").hide();	
+			 $("#rm_usersHead").hide();
+			 $("#rm_usersPage").hide();
+			 
+			 $("#rm_ID").empty();
+			 $("#rm_ID").append("Verschiedene Werte");
+			 $("#rm_Name").empty();
+			 $("#rm_Name").append("Verschiedene Werte");
+			 $("#rm_Type").empty();
+			 $("#rm_Type").append("Verschiedene Werte");
+		 }
 		 
-		 $("#rm_ID").empty();
-		 $("#rm_ID").append("Verschiedene Werte");
-		 $("#rm_Name").empty();
-		 $("#rm_Name").append("Verschiedene Werte");
-		 $("#rm_Type").empty();
-		 $("#rm_Type").append("Verschiedene Werte");
-	 }
-	 
-	 else{		 
+		/* Exactly one object selected */
+		 else{
+			 
+			 /* Replace current content of rightmanager sidebar */
+			 $("#rightmanager").replaceWith("<div id=\"rightmanager\" class = \"jDesktopInspector\">"+rm_Content+"</div>");
+			 			 
+			 $("#rm_rolesHead").show();
+			 $("#rm_rolesPage").show();
+			 $("#rm_rightsHead").show();
+			 $("#rm_rightsPage").show();	
+			 $("#rm_usersHead").show();
+			 $("#rm_usersPage").show();
+			 
+			 $("#rm_ID").empty();
+			 $("#rm_ID").append(theObject.id);
+			 $("#rm_Name").empty();
+			 $("#rm_Name").append(theObject.getAttribute("name"));
+			 $("#rm_Type").empty();
+			 $("#rm_Type").append(theObject.type);
 		 
-		 $("#rm_rolesHead").show();
-		 $("#rm_rolesPage").show();
-		 $("#rm_rightsHead").show();
-		 $("#rm_rightsPage").show();	
-		 $("#rm_usersHead").show();
-		 $("#rm_usersPage").show();
-		 
-		 $("#rm_ID").empty();
-		 $("#rm_ID").append(theObject.id);
-		 $("#rm_Name").empty();
-		 $("#rm_Name").append(theObject.getAttribute("name"));
-		 $("#rm_Type").empty();
-		 $("#rm_Type").append(theObject.type);
-	 
-		 
-		 
-		  /* Get Roles of selected object and write the roles into combobox */
-		  Modules.UserManager.getRoles({id:1}, GUI.username, function(roles){
-			  $("#rm_roles").empty();
-			  roles.forEach(function(item){
-				  $("#rm_roles").append("<div class=\"jDesktopInspector_element\"><input type=\"radio\" value=\""+item.name+"\" name=\"rm_rolesRadio\">"+item.name+"</div>");
-	          });		 
-		  });
-		  
-		  /* TODO: Get rights */
-		  
-		  
-		  
-		  /* TODO: Get users */
-	 }
+			 
+			 
+			  /* Get Roles of selected object and write the roles into combobox */
+			  Modules.UserManager.getRoles({id:1}, GUI.username, function(roles){
+				  $("#rm_roles").empty();
+				  roles.forEach(function(item){
+					  $("#rm_roles").append("<div class=\"jDesktopInspector_element\"><input type=\"radio\" value=\""+item.name+"\" name=\"rm_rolesRadio\">"+item.name+"</div>");
+		          });		 
+			  });
+			  
+			  /* TODO: Get rights */
+			  
+			  
+			  
+			  /* TODO: Get users */
+		 }
 	 
   };
 };
