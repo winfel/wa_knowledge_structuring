02039a8f20ffa8cbcc71dcb41db2b2f7a9f877d1
Patrick Baumann <pbaumann@mail.upb.de>
2014-04-02 14:50:40 +0200

    Sidebar Right manager: Fixed some accidental event removal bug :). Really technical...

--- source/Client/guis.common/javascript/1.rightmanager.js
+++ source/Client/guis.common/javascript/1.rightmanager.js
@@ -5,11 +5,17 @@
 GUI.rightmanager = new function() {
   var that = this;
 
+  var containerSelected;
+  var containerNoneSelected;
+
   /* Content of rightmanager sidebar*/
   var rm_Content;
 
   this.init = function() {
     console.log("GUI.rightmanager initialized");
+
+    this.containerSelected = $('#rightmanager .rightmanagerSelected');
+    this.containerNoneSelected = $('#rightmanager .rightmanagerNoneSelected');
 
     /* Add user event */
     $("#rmNewUserButton").click(function(event) {
@@ -27,18 +33,7 @@ GUI.rightmanager = new function() {
     });
 
     /* Initially no object is selected */
-    rm_Content = $('#rightmanager').html();
-    $("#rightmanager").replaceWith("<div class = \"jDesktopInspector\" id=\"rightmanager\">" +
-			"<div class=\"jDesktopInspector_main\">" +
-				"<div class=\"jDesktopInspector_pageHead\">Object:</div>" +
-				"<div class=\"jDesktopInspector_page jDesktopInspector_page_0\">" +
-					"<div class=\"jDesktopInspector_section\">" +
-						"<div class=\"jDesktopInspector_element\">No Object selected!</div>" +
-					"</div>" +
-				"</div>" +
-			"</div>" +
-		"</div>");
-
+    this.containerSelected.hide();
 
   };
 
@@ -52,28 +47,12 @@ GUI.rightmanager = new function() {
 
     /* No object selected */
     if (selectedObjects.length == 0) {
-
-      /* Store current content of rightmanager sidebar and replace it */
-      rm_Content = $('#rightmanager').html();
-      $("#rightmanager").replaceWith("<div class = \"jDesktopInspector\" id=\"rightmanager\">" +
-			"<div class=\"jDesktopInspector_main\">" +
-				"<div class=\"jDesktopInspector_pageHead\">Object:</div>" +
-				"<div class=\"jDesktopInspector_page jDesktopInspector_page_0\">" +
-					"<div class=\"jDesktopInspector_section\">" +
-						"<div class=\"jDesktopInspector_element\">No Object selected!</div>" +
-					"</div>" +
-				"</div>" +
-			"</div>" +
-		"</div>");
-
+      this.containerSelected.hide();
+      this.containerNoneSelected.show();
     }
 
     /* More than one object selected */
     else if (selectedObjects.length > 1) {
-
-      /* Replace current content of rightmanager sidebar */
-      $("#rightmanager").replaceWith("<div id=\"rightmanager\" class = \"jDesktopInspector\">" + rm_Content + "</div>");
-
       $("#rm_rolesHead").hide();
       $("#rm_rolesPage").hide();
       $("#rm_rightsHead").hide();
@@ -87,14 +66,13 @@ GUI.rightmanager = new function() {
       $("#rm_Name").append("Verschiedene Werte");
       $("#rm_Type").empty();
       $("#rm_Type").append("Verschiedene Werte");
+
+      this.containerSelected.show();
+      this.containerNoneSelected.hide();
     }
 
     /* Exactly one object selected */
     else {
-
-      /* Replace current content of rightmanager sidebar */
-      $("#rightmanager").replaceWith("<div id=\"rightmanager\" class = \"jDesktopInspector\">" + rm_Content + "</div>");
-
       $("#rm_rolesHead").show();
       $("#rm_rolesPage").show();
       $("#rm_rightsHead").show();
@@ -110,7 +88,6 @@ GUI.rightmanager = new function() {
       $("#rm_Type").append(theObject.type);
 
 
-
       /* Get Roles of selected object and write the roles into combobox */
       Modules.UserManager.getRoles({id: 1}, GUI.username, function(roles) {
         $("#rm_roles").empty();
@@ -124,8 +101,12 @@ GUI.rightmanager = new function() {
 
 
       /* TODO: Get users */
-    }
 
+
+
+      this.containerSelected.show();
+      this.containerNoneSelected.hide();
+    }
   };
 };
 
