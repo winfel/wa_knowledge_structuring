03c3425aea35b807796d735c4ee5fc18c1d9cb7e
Lisa Bunse <lbunse@mail.upb.de>
2014-04-20 12:40:27 +0200

    Right manager Dialog: use getRolesForObjects function and fixed errors

--- source/Client/guis.common/javascript/1.rightmanagerDialog.js
+++ source/Client/guis.common/javascript/1.rightmanagerDialog.js
@@ -15,46 +15,39 @@ GUI.rightmanagerDialog = new function() {
 
   this.init = function() {
     console.log("GUI.rightmanagerDialog initialized");
+    var j=1;
 
-    for (var i = 0; i < roles.length; i++) {          
-           var j= i+1;
-           listitems += "<li><a href='#tabs-"+j+"'>"+roles[i]+"</a></li>";
-           
- 
-           tabpages += "<div id='tabs-"+j+"'>";
-           
-           //content of tabpages
-           /*
-           Modules.RightManager.getRolesForObject({id: 1"}, function(roles) {
-               roles.forEach(function(role) {
-                 console.log(role);
+     Modules.RightManager.getRolesForObject({id: 1, type: "PaperObject"}, function(roles2) {
+               roles2.forEach(function(role) {
+                    listitems += "<li><a href='#tabs-"+j+"'>"+role.name+"</a></li>";
+                    tabpages += "<div id='tabs-"+j+"'>";
+                    // console.log(role);
+                      tabpages +=  "</div>";
+                 
+                       $("#rmdTabList").append(listitems);
+                       
+                      $("#rmdTabs").append(tabpages);
+               
+                      listitems="";
+                      tabpages = "";
+                      j++;
                });
+
+                 //Append Plus Tab
+                $("#rmdTabList").append('<li class="rmdTabPage_add"><a href="#rmdTabPage_add">+</a></li>');
+                //Set content for Plus Tab
+                $("#rmdTabs").append('<div id="rmdTabPage_add">Bla</div>');
+
+                // Create the tabs
+                this.rmdTabs = $("#rmdTabs").tabs();
+
            });
-           */
- 
-           tabpages +=  "</div>";
-             
-         $("#rmdTabList").append(listitems);
-         
-        $("#rmdTabs").append(tabpages);
- 
-        listitems="";
-        tabpages = "";
-     };
- 
- 
-     //Append Plus Tab
-     $("#rmdTabList").append('<li class="rmdTabPage_add"><a href="#rmdTabPage_add">+</a></li>');
-     //Set content for Plus Tab
-     $("#rmdTabs").append('<div id="rmdTabPage_add">Bla</div>')
 
 
-    // Create the tabs
-    this.rmdTabs = $("#rmdTabs").tabs();
 
     // Create the dialog
     this.rmd = $("#rightmanagerDialog").dialog({
-      title: "Right Manager Setupkgjfjf",
+      title: "Right Manager Setup",
       autoOpen: false,
       modal: true,
       width: 500,
