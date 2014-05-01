94718f216ca4ec06a3d42a49865e2e20af2738d0
Patrick Baumann <pbaumann@mail.upb.de>
2014-05-01 14:52:34 +0200

    Fixed an issue that occurred if one client called the same server function within a short amount of time.

--- source/Client/Dispatcher.js
+++ source/Client/Dispatcher.js
@@ -27,22 +27,6 @@ var responseCleanupTimeout = false;
 Dispatcher.counter = 1;
 
 /**
- * Manages a call from the server
- * 
- * @param {Object} message the received from the server.
- */
-Dispatcher.call = function(message) {
-  var type = message.name;
-  var data = message.data;
-
-  if (calls[type]) {
-    calls[type](data);
-  } else {
-    console.log('ERROR: No function for ' + type);
-  }
-};
-
-/**
  * Manages a response from the server
  * 
  * @param {Object} message the received from the server.
@@ -83,6 +67,34 @@ Dispatcher.query = function(queryName, queryArguments, responseFunction) {
 };
 
 /**
+ * Manages a call from the server
+ * 
+ * @param {Object} message the received from the server.
+ */
+Dispatcher.call = function(message) {
+  var type = message.name;
+  var data = message.data;
+
+  if (calls[type]) {
+    if ($.isArray(calls[type])) {
+      // If it is an array, get the latest element on remove it from the array and call it.
+      var callback = calls[type].pop();
+      callback(data);
+
+      // If only one item is left, remove the array and store callback function itself.
+      if (calls[type].length == 1)
+        calls[type] = calls[type].pop();
+      
+    } else {
+      // The previous behavior
+      calls[type](data);
+    }
+  } else {
+    console.log('ERROR: No function for ' + type);
+  }
+};
+
+/**
  * Register a new callfunction by type
  * 
  * @param {Object} type the type of call function
@@ -90,16 +102,31 @@ Dispatcher.query = function(queryName, queryArguments, responseFunction) {
  */
 Dispatcher.registerCall = function(type, callFunction) {
   //callfunction signature (socket,data);
-  calls[type] = callFunction;
+  if (calls[type]) {
+    // Allows multiple callback functions at the same time.
+    // TCP takes care of that fact that messages from one client are
+    // handled in the order which they have been initially sent.
+    if (!$.isArray(calls[type])) {
+      calls[type] = [calls[type]];
+    }
+    // Add the callback to the beginning of the queue.
+    calls[type].splice(0, 0, callFunction);
+  } else {
+    // Fallback to the previous behavior
+    calls[type] = callFunction;
+  }
 };
 
 /**
- * Removes a callfunction for a certain type
+ * Removes a callfunction for a certain type, if there is no array item left.
  * 
  * @param {Object} type the type of call function
  */
 Dispatcher.removeCall = function(type) {
-  delete calls[type];
+  // Only remove the callback if no other element is left
+  if (!$.isArray(calls[type])) {
+    delete calls[type];
+  }
 };
 
 /**
