50406aa5f74bc5342f7dda13c8818c29fb4bc754
Ivan Jovanovikj <jovan.ivan.88@gmail.com>
2014-05-20 15:14:30 +0200

    MongoDBConnector: added new empty functions

--- source/Server/connectors/MongoDBConnector.js
+++ source/Server/connectors/MongoDBConnector.js
@@ -199,4 +199,166 @@ mongoConnector.remove = function(roomID, objectID, context, callback) {
     console.log("ALEX mongoConnector.remove");
 }
 
+/**
+*	create a new object on the persistence layer
+*	to direcly work on the new object, specify a callback function
+*	after(objectID)
+*	@function createObject
+*	@param roomID
+*	@param type
+*	@param data
+*	@param context
+*	@param callback
+*
+*/
+fileConnector.createObject = function(roomID, type, data, context, callback) {
+	 console.log("ALEX mongoConnector.createObject");
+}
+
+/**
+*	duplicate an object on the persistence layer
+*	to directly work on the new object, specify an after function
+*	after(objectID)
+* 	@function duplicateObject
+*	@param roomID
+*	@param toRoom
+*	@param objectID
+*	@param context
+*	@param callback
+*
+*/
+fileConnector.duplicateObject = function(roomID, toRoom, objectID, context, callback) {
+	console.log("ALEX mongoConnector.duplicateObject");
+}
+
+/**
+*	returns the attribute set of an object
+* 	@function getObjectData
+*	@param roomID
+*	@param objectID
+*	@param context
+*/
+fileConnector.getObjectData = function(roomID, objectID, context) {
+	console.log("ALEX mongoConnector.getObjectData");
+}
+
+/**
+*	internal
+*	read an object file and return the attribute set
+*	@function getObjectDataByFile
+*	@param roomID
+*	@param objectID
+*/
+fileConnector.getObjectDataByFile = function(roomID, objectID) {
+	console.log("ALEX mongoConnector.getObjectDataByFile");
+}
+
+/**
+*	@function trimImage
+*	@param roomID
+*	@param objectID
+*	@param context
+*	@param callback
+*/
+fileConnector.trimImage = function(roomID, objectID, context, callback) {
+	console.log("ALEX mongoConnector.trimImage");
+};
+
+/**
+*	@function isInlineDisplayable
+*	@param mimeType
+*/
+fileConnector.isInlineDisplayable = function(mimeType) {
+	console.log("ALEX mongoConnector.isInlineDisplayable");
+}
+
+/**
+*	@function getMimeTyp
+*	@param roomID
+*	@param objectID
+*	@param context
+*	@param callback
+*/
+fileConnector.getMimeType = function(roomID, objectID, context, callback) {
+	console.log("ALEX mongoConnector.getMimeType");
+}
+
+/**
+* SYNC
+*	@function getInlinePreviewProviderName
+*	@param mimeType
+*/
+fileConnector.getInlinePreviewProviderName = function(mimeType) {
+	console.log("ALEX mongoConnector.getInlinePreviewProviderName");
+}
+
+/**
+* SYNC
+*	@function getInlinePreviewMimeTypes
+*/
+fileConnector.getInlinePreviewMimeTypes = function() {
+	console.log("ALEX mongoConnector.getInlinePreviewMimeTypes");
+}
+
+/**
+* SYNC
+*	@function getInlinePreviewProviders
+*/
+fileConnector.getInlinePreviewProviders = function() {
+	console.log("ALEX mongoConnector.getInlinePreviewProviders");
+	return {
+		//"application/pdf" : "pdf",
+		"image/jpeg" : "image",
+		"image/jpg" : "image",
+		"image/png" : "image",
+		"image/gif" : "image",
+		"image/bmp" : "image",
+		"image/x-bmp" : "image",
+		"image/x-ms-bmp" : "image"
+	}
+}
+
+/**
+*	@function getInlinePreviewDimensions
+*	@param roomID
+*	@param objectID
+*	@param mimeType
+*	@param context
+*	@param callback
+*/
+fileConnector.getInlinePreviewDimensions = function(roomID, objectID, mimeType, context, callback) {
+	console.log("ALEX mongoConnector.getInlinePreviewDimensions");
+}
+
+/**
+*	@function getInlinePreview
+*	@param roomID
+*	@param objectID
+*	@param mimeType
+*	@param context
+*	@param callback
+*/
+fileConnector.getInlinePreview = function(roomID, objectID, mimeType, context, callback) {
+	console.log("ALEX mongoConnector.getInlinePreview");
+}
+
+/**
+*	@function getInlinePreviewMimeType
+*	@param roomID
+*	@param objectID
+*	@param context
+*	@param callback
+*/
+fileConnector.getInlinePreviewMimeType = function(roomID, objectID, context, callback) {
+	console.log("ALEX mongoConnector.getInlinePreviewMimeType");
+}
+
+/**
+* Head function and some subfunctions included, TODO JSDoc
+*	@function inlinePreviewProviders
+*/
+fileConnector.inlinePreviewProviders = {
+		console.log("ALEX mongoConnector.inlinePreviewProviders");
+}
+
 module.exports = mongoConnector;
