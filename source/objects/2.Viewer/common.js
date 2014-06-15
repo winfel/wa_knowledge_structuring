611f4e75c80bd2dddfef9dea63fb30b5f3a83c03
Patrick Baumann <pbaumann@mail.upb.de>
2014-06-15 17:07:28 +0200

    Document Viewer: Basic.. Nothing special.

---
+++ source/objects/2.Viewer/common.js
@@ -0,0 +1,38 @@
+/**
+*    Webarena - A web application for responsive graphical knowledge work
+*
+*    @author Felix Winkelnkemper, University of Paderborn, 2012
+*
+*/
+
+var Modules=require('../../server.js');
+
+var Viewer=Object.create(Modules.ObjectManager.getPrototype('GeneralObject'));
+
+Viewer.register=function(type){
+	
+	// Registering the object
+	
+	GeneralObject=Modules.ObjectManager.getPrototype('GeneralObject');
+	GeneralObject.register.call(this,type);
+	
+  this.registerAttribute('file', {type:'text', standard:'[somefileid]'});
+	
+	this.standardData.width=210*3;
+	this.standardData.height=297*3;
+	
+};
+
+//set restrictedMovingArea to true, if you want to enable interface interaction within
+//the HTML element. This is useful if you want to use buttons, links or even canvas elements.
+//when set to true, you must specify an area where the object can be moved. This area must
+//have its class set to "moveArea". Set restrictedMovingArea to false if you use the HTML
+//element for diplaying purposes only.
+
+Viewer.restrictedMovingArea = true;
+Viewer.isCreatable=true;
+Viewer.category='Texts';
+
+Viewer.register('Viewer');
+
+module.exports=Viewer;
---
+++ source/objects/2.Viewer/view.js
@@ -0,0 +1,91 @@
+/**
+ *    Webarena - A webclient for responsive graphical knowledge work
+ *
+ *    @author Felix Winkelnkemper, University of Paderborn, 2011
+ *
+ */
+
+
+Viewer.draw = function(external) {
+
+  var rep = this.getRepresentation();
+
+  this.drawDimensions(external);
+
+  this.setViewWidth(this.getAttribute('width'));
+  this.setViewHeight(this.getAttribute('height'));
+
+  $(rep).attr("layer", this.getAttribute('layer'));
+
+  var that = this;
+
+  //this.updateContent();
+  return rep;
+};
+
+/*
+ Viewer.updateContent = function() {
+ 
+ var rep = this.getRepresentation();
+ 
+ this.getContentAsString(function(text) {
+ 
+ if (text != that.oldContent) {
+ $(rep).find("body").html(text);
+ }
+ 
+ that.oldContent = text;
+ 
+ });
+ 
+ };*/
+
+Viewer.createRepresentation = function(parent) {
+
+  console.log("CALLED!!!!!");
+
+  var rep = GUI.svg.other(parent, "foreignObject");
+  rep.dataObject = this;
+  var $rep = $(rep);
+
+  var body = document.createElement("body");
+  $rep.attr({id: this.getAttribute('id')});
+  $rep.append(body);
+
+  var $viewer = $(body).addClass('paperWriter');
+  
+  var moveArea = $("<div>");
+  moveArea.addClass("moveArea");
+  $viewer.append(moveArea);
+  
+  /* IFRAME */
+//  pWriter.append("<iframe></iframe>");
+//  
+//  var iFrame = $(pWriter.find("iframe"));
+//  var iframe_loaded = false;
+//  iFrame.one('load', function() {
+//    iframe_loaded = true;
+//  });
+//
+//  iFrame.attr('src', 'http://' + window.location.hostname + ':8080/getPaper/public/cd7e6155-3a12-49c7-9bbd-a8e3098bd65d.html/');
+
+  /* IN HTML */
+  var request = $.ajax({
+    url: 'http://' + window.location.hostname + ':8080/getPaper/public/cd7e6155-3a12-49c7-9bbd-a8e3098bd65d.html/',
+    cache: false
+  });
+
+  request.done(function(html) {
+    //$rep.append($(html).find("body").html());
+    var $tmp = $(html.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/g, ''));
+    $viewer.append($tmp);
+  });
+
+  request.fail(function(jqXHR, textStatus) {
+    console.log("I am sorry, I was not able to load the requested paper.");
+  });
+
+  this.initGUI(rep);
+
+  return rep;
+};
