ba194c0d79b0d3683e0fef2e9d3f3691a61281f2
Patrick Baumann <pbaumann@mail.upb.de>
2014-09-10 17:12:19 +0200

    Added rights and default roles for the viewer and subroom.

--- source/objects/2.Viewer/common.js
+++ source/objects/2.Viewer/common.js
@@ -2,7 +2,6 @@
  *    Webarena - A web application for responsive graphical knowledge work
  *
  *    @author Felix Winkelnkemper, University of Paderborn, 2012
- *
  */
 
 var Modules = require('../../server.js');
@@ -11,46 +10,55 @@ var Viewer = Object.create(Modules.ObjectManager.getPrototype('GeneralObject'));
 
 Viewer.register = function(type) {
   var that = this;
-  
+
   // Registering the object
   GeneralObject = Modules.ObjectManager.getPrototype('GeneralObject');
   GeneralObject.register.call(this, type);
 
+  // Registers the available rights ...
+  this.registerRight("create", "You may create new content within this viewer.");
+  this.registerRight("read",   "You may access the content within this viewer.");
+  this.registerRight("update", "You may change the content within this viewer.");
+  this.registerRight("delete", "You may delete the content within this viewer.");
+  // ... and default roles for this object.
+  this.registerDefaultRole("Reader", ["read"]);
+  this.registerDefaultRole("Coworker", ["create", "read"]);
+  
   this.registerAttribute('file', {type: 'text', changedFunction: function(object, value) {
       object.reloadDocument(value);
     }});
   this.registerAttribute('highlights', {type: 'text', standard: '', changedFunction: function(object, value) {
       object.loadHighlights();
     }});
-    
+
   Modules.Dispatcher.registerCall("dbDocument_comments", function(data) {
     that.addComment(data.user, data.id || data._id, data.data);
   });
-  
+
   Modules.Dispatcher.registerCall("dbDocument_comments_audio", function(data) {
     that.addComment(data.user, data.id || data._id, data.data);
   });
-  
+
   Modules.Dispatcher.registerCall("dbDocumentAdded_comments", function(data) {
     that.addComment(data.user, data.id || data._id, data.data);
   });
-  
+
   Modules.Dispatcher.registerCall("dbDocumentAdded_comments_audio", function(data) {
     that.addComment(data.user, data.id || data._id, data.data);
   });
-  
+
   Modules.Dispatcher.registerCall("dbDocumentRemoved_comments", function(data) {
     that.removeComment(data.id || data._id, "comment", "commented");
   });
-  
+
   Modules.Dispatcher.registerCall("dbDocumentRemoved_comments_audio", function(data) {
     that.removeComment(data.id || data._id, "audioobject", "audio");
   });
-  
+
   Modules.Dispatcher.registerCall("dbAllDocumentsSend_comments", function(data) {
     that.updateStatus();
   });
-  
+
   Modules.Dispatcher.registerCall("dbAllDocumentsSend_comments_audio", function(data) {
     that.updateStatus();
   });
--- source/objects/6.Subroom/common.js
+++ source/objects/6.Subroom/common.js
@@ -1,11 +1,11 @@
 /**
-*    Webarena - A web application for responsive graphical knowledge work
-*
-*    @author Felix Winkelnkemper, University of Paderborn, 2012
-*
-*/
+ *    Webarena - A web application for responsive graphical knowledge work
+ *
+ *    @author Felix Winkelnkemper, University of Paderborn, 2012
+ *
+ */
 
-var Modules=require('../../server.js');
+var Modules = require('../../server.js');
 
 /**
  * Subroom
@@ -13,7 +13,7 @@ var Modules=require('../../server.js');
  * @classdesc Common elements for view and server
  */
 
-var Subroom=Object.create(Modules.ObjectManager.getPrototype('IconObject'));
+var Subroom = Object.create(Modules.ObjectManager.getPrototype('IconObject'));
 
 /**
  * Registers the object (actions).
@@ -24,28 +24,34 @@ var Subroom=Object.create(Modules.ObjectManager.getPrototype('IconObject'));
  * @see objects/1.GeneralObject/common.js
  * @param {string} type The type of the object
  */
-Subroom.register=function(type){
-	
-	// Registering the object
-	
-	IconObject=Modules.ObjectManager.getPrototype('IconObject');
-	IconObject.register.call(this,type);
-	
-	var self=this;
-	
-	this.registerAction('Follow',function(object){
-		
-		object.execute();
-		
-	},true);
-	
-	this.registerAction('Open in new window',function(object){
-		
-		object.execute(true);
-		
-	},true);
-	
-	this.registerAction('Store in Tab-List', function(object) {
+Subroom.register = function(type) {
+
+  // Registering the object
+
+  IconObject = Modules.ObjectManager.getPrototype('IconObject');
+  IconObject.register.call(this, type);
+
+  // Registers the available rights ...
+  this.registerRight("manage", "A user may manage this subroom.");
+  this.registerRight("enter", "A user may enter this subroom.");
+  // ... and default roles for this object.
+  this.registerDefaultRole("Coworker", ["enter"]);
+
+  var self = this;
+
+  this.registerAction('Follow', function(object) {
+
+    object.execute();
+
+  }, true);
+
+  this.registerAction('Open in new window', function(object) {
+
+    object.execute(true);
+
+  }, true);
+
+  this.registerAction('Store in Tab-List', function(object) {
     var destination = object.getAttribute('destination');
 
     if (!destination) {
@@ -55,7 +61,7 @@ Subroom.register=function(type){
       destination = random;
     }
 
-    GUI.tabs.addTab(object.getAttribute('name')+" (Room)",object.getAttribute('destination'),object.id);
+    GUI.tabs.addTab(object.getAttribute('name') + " (Room)", object.getAttribute('destination'), object.id);
     GUI.tabs.redrawTabContent();
 
   }, true);
@@ -70,44 +76,44 @@ Subroom.register=function(type){
  * @see objects/1.GeneralObject/common.js
  * @param {boolean} openInNewWindow
  */
- Subroom.execute = function(openInNewWindow) {
-    
-    var destination = this.getAttribute('destination');
-    var that = this;
+Subroom.execute = function(openInNewWindow) {
 
-    // TODO this must be done serverside in the connector
-    if (!destination) {
-        var random = new Date().getTime() - 1296055327011;        
-        this.setAttribute('destination', random.toString());
-        destination = random;
-        
-        GUI.tabs.addTab(this.getAttribute('name')+" (Room)",this.getAttribute('destination'),this.id);
-        GUI.tabs.redrawTabContent();
-    }
+  var destination = this.getAttribute('destination');
+  var that = this;
 
-    if (openInNewWindow) {
-    	GUI.tabs.addTab(this.getAttribute('name')+" (Room)",this.getAttribute('destination'),this.id);
-        GUI.tabs.redrawTabContent();
-        window.open(destination);
+  // TODO this must be done serverside in the connector
+  if (!destination) {
+    var random = new Date().getTime() - 1296055327011;
+    this.setAttribute('destination', random.toString());
+    destination = random;
 
-    } else {
-        GUI.tabs.addTab(this.getAttribute('name')+" (Room)",this.getAttribute('destination'),this.id);
-        GUI.tabs.redrawTabContent();
-        Modules.RightManager.hasAccess("read", { id: this.id, type: this.type}, GUI.username, function(result) {
-          if(result) {
-            ObjectManager.loadRoom(destination, false, ObjectManager.getIndexOfObject(that.getAttribute('id')));
-        }else{
-            var audio = new Audio('/guis.common/sounds/cant_touch_this.mp3');
-            audio.play();
-        }
-        });
-        
-    }
-}
+    GUI.tabs.addTab(this.getAttribute('name') + " (Room)", this.getAttribute('destination'), this.id);
+    GUI.tabs.redrawTabContent();
+  }
+
+  if (openInNewWindow) {
+    GUI.tabs.addTab(this.getAttribute('name') + " (Room)", this.getAttribute('destination'), this.id);
+    GUI.tabs.redrawTabContent();
+    window.open(destination);
+
+  } else {
+    GUI.tabs.addTab(this.getAttribute('name') + " (Room)", this.getAttribute('destination'), this.id);
+    GUI.tabs.redrawTabContent();
+
+    that.hasAccess("enter", function(result) {
+      if (result) {
+        ObjectManager.loadRoom(destination, false, ObjectManager.getIndexOfObject(that.getAttribute('id')));
+      } else {
+        var audio = new Audio('/guis.common/sounds/cant_touch_this.mp3');
+        audio.play();
+      }
+    });
+  }
+};
 
 Subroom.register('Subroom');
-Subroom.isCreatable=true;
+Subroom.isCreatable = true;
 
-Subroom.category='Rooms';
+Subroom.category = 'Rooms';
 
-module.exports=Subroom;
+module.exports = Subroom;
